b49d223fb2227451d981827caee264bf1bbfb8fa
Estelle-dng <estelle.denage@lapiscine.pro>
2021-06-16 14:27:14 +0200

    add entities

---
+++ src/Entity/Ingredient.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\IngredientRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=IngredientRepository::class)
+ */
+class Ingredient
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $name;
+
+    /**
+     * @ORM\ManyToMany(targetEntity=Plate::class, inversedBy="ingredients")
+     */
+    private $Plate;
+
+    public function __construct()
+    {
+        $this->Plate = new ArrayCollection();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection|Plate[]
+     */
+    public function getPlate(): Collection
+    {
+        return $this->Plate;
+    }
+
+    public function addPlate(Plate $plate): self
+    {
+        if (!$this->Plate->contains($plate)) {
+            $this->Plate[] = $plate;
+        }
+
+        return $this;
+    }
+
+    public function removePlate(Plate $plate): self
+    {
+        $this->Plate->removeElement($plate);
+
+        return $this;
+    }
+}
---
+++ src/Entity/Plate.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\PlateRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=PlateRepository::class)
+ */
+class Plate
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $name;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $ImgUrl;
+
+    /**
+     * @ORM\ManyToMany(targetEntity=Ingredient::class, mappedBy="Plate")
+     */
+    private $ingredients;
+
+    public function __construct()
+    {
+        $this->ingredients = new ArrayCollection();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getImgUrl(): ?string
+    {
+        return $this->ImgUrl;
+    }
+
+    public function setImgUrl(?string $ImgUrl): self
+    {
+        $this->ImgUrl = $ImgUrl;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection|Ingredient[]
+     */
+    public function getIngredients(): Collection
+    {
+        return $this->ingredients;
+    }
+
+    public function addIngredient(Ingredient $ingredient): self
+    {
+        if (!$this->ingredients->contains($ingredient)) {
+            $this->ingredients[] = $ingredient;
+            $ingredient->addPlate($this);
+        }
+
+        return $this;
+    }
+
+    public function removeIngredient(Ingredient $ingredient): self
+    {
+        if ($this->ingredients->removeElement($ingredient)) {
+            $ingredient->removePlate($this);
+        }
+
+        return $this;
+    }
+}
